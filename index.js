5d06e54ef6c46c9a2895b89275e6ea7a6e4fb608
Tavian Taylor <taviantaylor00@gmail.com>
2021-07-25 11:20:19 +0100

    messages and responses

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 const dotenv = require('dotenv')
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const {prefix, token} = require('./config.json');
+
 dotenv.config()
-//client.login(process.env.TOKEN);
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+	if (message.content === `${prefix}ping`) {
+		message.channel.send('Pong.');
+	} else if (message.content === `${prefix}beep`) {
+		message.channel.send('Boop.');
+	} else if (message.content === `${prefix}server`) {
+		message.channel.send(`This server's name is: ${message.guild.name}`);
+	} else if (message.content === `${prefix}user-info`) {
+		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+	}
+});
+client.login(token);
