003d5aa882719694ae7c40085de3e31514dba4db
Tavian Taylor <taviantaylor00@gmail.com>
2021-07-31 17:01:22 +0100

    fixing whitespace

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ for (const file of commandFiles) {
 
 client.once('ready', () => {
 	console.log('Ready!');
-     new_game = new game.Game();
+    new_game = new game.Game();
 });
 
 client.on('message', message => {
